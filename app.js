6f341b027cbf006b54b58e39510d749d36b078d0
agildav <agildav@gmail.com>
2018-10-22 17:55:24 -0400

    Added CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(
@@ -7,6 +8,7 @@ app.use(
     extended: true
   })
 );
+app.use(cors());
 
 //  Routes
 const productRoutes = require("./api/routes/products");
