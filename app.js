8441becc51602e7f3979e65e82166b13c3141c32
agildav <agildav@gmail.com>
2018-10-22 17:37:17 -0400

    Added order post method

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
 //  Routes
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
