3d7dc2e12485054e61b5541428735aa9dda8f9d4
agildav <agildav@gmail.com>
2018-11-06 15:42:29 -0400

    Added get all orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,9 +4,31 @@ const mongoose = require("mongoose");
 const Order = require("../models/order");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders were fetched"
-  });
+  Order.find()
+    .select("-__v")
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "/orders/" + doc._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "Could not get orders"
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
